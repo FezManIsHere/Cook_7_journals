99b15c7b8bbf56ec54b5747e37f7541502126c69
Carter Cook <alonzi.alonzo7@gmail.com>
2015-09-29 10:57:06 -0500

    NetBeans Stuff

---
+++ NetBeansProjects/Cook_7_evens/src/cook_7_evens/Cook_7_evens.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cook_7_evens;
+
+/**
+ *
+ * @author Ramborux
+ */
+public class Cook_7_evens {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < 100; i++){
+            
+//            if(i % 2 == 0) {
+//                System.out.println(i + " is even");
+//                even++;
+//            }else{
+//                System.out.println(i + " is odd");
+//                odd++;
+//            }
+//            
+        }
+//        System.out.println(even + " numbers are even");
+//        System.out.println(odd + " numbers are odd");
+            
+    /*
+        A method is like a little program.
+        A mmethod is an action you want to repeat
+        frequently.
+        */
+        
+    
+    }
+    boolean isEven (int somenumber) {
+        return (somenumber % 2 == 0);
+    }
+    boolean isOdd (int somenumber) {
+        return (somenumber % 2 == 1);
+    }
+}
---
+++ NetBeansProjects/Cook_7_primes/src/cook_7_primes/Cook_7_primes.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cook_7_primes;
+
+/**
+ *
+ * @author Ramborux
+ */
+public class Cook_7_primes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            for (int i = 0; i < 101; i++){
+                if (i % 2 != 0) {
+                    if (i % 5 != 0){
+                    }
+            }
+            }
+    }
+    
+
+
+
+
+
+
+}
