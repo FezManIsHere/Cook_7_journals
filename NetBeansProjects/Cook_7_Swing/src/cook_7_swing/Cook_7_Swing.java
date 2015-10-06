aea8b0fd96dcd1a8d63b75eacd052c924378cbda
Carter Cook <alonzi.alonzo7@gmail.com>
2015-10-06 11:17:00 -0500

    Updates

---
+++ NetBeansProjects/Cook_7_Swing/src/cook_7_swing/Cook_7_Swing.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cook_7_swing;
+
+/**
+ *
+ * @author Ramborux
+ */
+public class Cook_7_Swing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
