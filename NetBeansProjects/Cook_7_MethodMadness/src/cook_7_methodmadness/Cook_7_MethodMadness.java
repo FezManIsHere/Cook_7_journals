59eb477ec19566ccbfbff15f446f02c7881d0d1b
Carter Cook <alonzi.alonzo7@gmail.com>
2015-10-08 10:27:19 -0500

    Updates

--- NetBeansProjects/Cook_7_MethodMadness/src/cook_7_methodmadness/Cook_7_MethodMadness.java
+++ NetBeansProjects/Cook_7_MethodMadness/src/cook_7_methodmadness/Cook_7_MethodMadness.java
@@ -12,7 +12,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.stage.Stage;
 /**
  *
  * @author Ramborux
@@ -21,21 +27,6 @@ public class Cook_7_MethodMadness extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
