145536c9d36041e8a5e71ac4b24a2ad1346b6655
Carter Cook <alonzi.alonzo7@gmail.com>
2015-10-20 16:14:09 -0500

    updates

--- NetBeansProjects/Cook_7_MethodMadness/src/cook_20_methodmadness/Cook_20_MethodMadness.java
+++ NetBeansProjects/Cook_7_MethodMadness/src/cook_20_methodmadness/Cook_20_MethodMadness.java
@@ -34,8 +34,10 @@ public class Cook_20_MethodMadness extends Application {
         Group root = new Group();
         Canvas canvas = new Canvas(1000, 1000);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawShapes(gc);
+        drawBackground(gc);
         drawCircle((int) canvas.getWidth(),(int) canvas.getHeight(), 500, canvas, gc);
+        drawShapes(gc);
+     
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
@@ -48,63 +50,96 @@ public class Cook_20_MethodMadness extends Application {
      gc.setFill(Color.GREEN);
      gc.fillOval(10, 60, 30, 30);
      }*/
+    private void drawBackground(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 1000, 1000);
+    }
     public static void drawShapes(GraphicsContext gc) {
         int realx = 0;
         int realy = 0;
+        int fakex = 1000;
+        int fakey = 0;
         for (int i = 0; i < 1001; i++) {
             if (isDivisiblebytwo(i) & isDivisiblebythree(i) & isDivisiblebyfive(i)) {
                 gc.setFill(Color.BEIGE);
                 gc.fillOval(realx, realy, 20, 20);
+                gc.fillOval(fakex, fakey, 20, 20);
                 realx = realx + 2;
                 realy = realy + 2;
+                fakex = fakex - 2;
+                fakey = fakey + 2;
             } else {
                 if (isDivisiblebytwo(i) & isDivisiblebythree(i)) {
                     gc.setFill(Color.RED);
                     gc.fillOval(realx, realy, 20, 20);
+                    gc.fillOval(fakex, fakey, 20, 20);
                     realx = realx + 2;
                     realy = realy + 2;
+                    fakex = fakex - 2;
+                    fakey = fakey + 2;
                 } else {
                     if (isDivisiblebythree(i) & isDivisiblebyfive(i)) {
                         gc.setFill(Color.ORANGE);
                         gc.fillOval(realx, realy, 20, 20);
+                        gc.fillOval(fakex, fakey, 20, 20);
                         realx = realx + 2;
                         realy = realy + 2;
+                        fakex = fakex - 2;
+                        fakey = fakey + 2;
                     } else {
                         if (isDivisiblebyeleven(i) & isDivisiblebyfive(i)) {
                             gc.setFill(Color.YELLOW);
                             gc.fillOval(realx, realy, 20, 20);
+                            gc.fillOval(fakex, fakey, 20, 20);
                             realx = realx + 2;
                             realy = realy + 2;
+                            fakex = fakex - 2;
+                            fakey = fakey + 2;
                         } else {
                             if (isDivisiblebyeleven(i) & isDivisiblebythree(i)) {
                                 gc.setFill(Color.GREEN);
                                 gc.fillOval(realx, realy, 20, 20);
+                                gc.fillOval(fakex, fakey, 20, 20);
                                 realx = realx + 2;
                                 realy = realy + 2;
+                                fakex = fakex - 2;
+                                fakey = fakey + 2;
                             } else {
                                 if (isDivisiblebyeleven(i) & isDivisiblebytwo(i)) {
                                     gc.setFill(Color.AQUA);
                                     gc.fillOval(realx, realy, 20, 20);
+                                    gc.fillOval(fakex, fakey, 20, 20);
                                     realx = realx + 2;
                                     realy = realy + 2;
+                                    fakex = fakex - 2;
+                                    fakey = fakey + 2;
                                 } else {
                                     if (isDivisiblebyfive(i)) {
                                         gc.setFill(Color.BLUE);
                                         gc.fillOval(realx, realy, 20, 20);
+                                        gc.fillOval(fakex, fakey, 20, 20);
                                         realx = realx + 2;
                                         realy = realy + 2;
+                                        fakex = fakex - 2;
+                                        fakey = fakey + 2;
                                     } else {
                                         if (isDivisiblebythree(i)) {
                                             gc.setFill(Color.PURPLE);
                                             gc.fillOval(realx, realy, 20, 20);
+                                            gc.fillOval(fakex, fakey, 20, 20);
                                             realx = realx + 2;
                                             realy = realy + 2;
+                                            fakex = fakex - 2;
+                                            fakey = fakey + 2;
                                         } else {
                                             if (isDivisiblebytwo(i)) {
                                                 gc.setFill(Color.BROWN);
                                                 gc.fillOval(realx, realy, 20, 20);
+                                                gc.fillOval(fakex, fakey, 20, 20);
                                                 realx = realx + 2;
                                                 realy = realy + 2;
+                                                fakex = fakex - 2;
+                                                fakey = fakey + 2;
                                             }
                                         }
                                     }
@@ -140,11 +175,14 @@ public class Cook_20_MethodMadness extends Application {
     }
     static void drawCircle(int x0, int y0, int radius, Canvas canvas, GraphicsContext gc){
 	  int x = radius;
-	  int y = 0;
+	  int y = -2000;
 	  int decisionOver2 = 1 - x;   // Decision criterion divided by 2 evaluated at x=r, y=0
 	  int imageWidth = (int) canvas.getWidth();
 	  int imageHeight = (int) canvas.getHeight();
-	 
+	  int r = 0;
+          int g = 0;
+          int b = 0;
+         
 	
 	
 	  while(x >= y){
@@ -157,8 +195,12 @@ public class Cook_20_MethodMadness extends Application {
 	    drawPixel( x + x0, -y + y0, gc);
 	    drawPixel( y + x0, -x + y0, gc);
 	    y++;
-            
-	    if (decisionOver2<=0){
+            gc.setFill(Color.rgb(r, g, b, .5));
+	    r = (r + 1)%255;
+            g = (g + 2)%255;
+            b = (b + 3)%255;
+             
+            if (decisionOver2<=0){
 	      decisionOver2 += 2 * y + 1;   // Change in decision criterion for y -> y+1
 	    }else{
 	      x--;
