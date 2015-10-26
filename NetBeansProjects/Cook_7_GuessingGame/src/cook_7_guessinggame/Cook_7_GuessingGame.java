76d6c0751598ba3e20e06b28fcf766eff231937c
Carter Cook <alonzi.alonzo7@gmail.com>
2015-10-26 16:13:24 -0500

    Updates

---
+++ NetBeansProjects/Cook_7_GuessingGame/src/cook_7_guessinggame/Cook_7_GuessingGame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cook_7_guessinggame;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author Ramborux
+ */
+public class Cook_7_GuessingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Guess a number from 1 to 10");
+    }
+    
+}
