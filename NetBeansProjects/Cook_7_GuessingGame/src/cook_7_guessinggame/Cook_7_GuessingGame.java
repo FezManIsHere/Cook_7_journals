6f203f2117a2c04ae5969bbbfaaad13aaca34d32
Carter Cook <alonzi.alonzo7@gmail.com>
2015-10-29 16:10:10 -0500

    Updates

--- NetBeansProjects/Cook_7_GuessingGame/src/cook_7_guessinggame/Cook_7_GuessingGame.java
+++ NetBeansProjects/Cook_7_GuessingGame/src/cook_7_guessinggame/Cook_7_GuessingGame.java
@@ -1,70 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cook_7_guessinggame;
 import java.util.Scanner;
 import java.util.Random;
-/**
- *
- * @author Ramborux
- */
+
 public class Cook_7_GuessingGame {
 
-    /**
-     * @param args the command line arguments
-     */
     static int realNumber;
     static int maybeNumber;
+    static String again;
+    static boolean playAgain = false;
     static String name;
     static Scanner scan = new Scanner(System.in);
     static Random why = new Random();
     
     public static void main(String[] args) {
-        playGame();
-        
-    }
+        playAgain = true;
+        getName();
+        while (playAgain) {
+            playGame();
+        }
+        }
+    
     private static void playGame() {
         int tries = 1;
         genNum();
-        getName();
         getGuess();
         checkNumber(tries);
     }
+    
     private static void genNum() {
         realNumber = why.nextInt(10);
     }
+    
     private static void getName() {
     System.out.println("What is your name?");
         name = scan.nextLine();
 }
+    
     private static void getGuess() {
         System.out.println("Guess a number from 1 to 10");
         maybeNumber = scan.nextInt();
 }
+    
     private static void checkNumber(int tries) {
         if(tries < 3 & maybeNumber != realNumber) {
-            System.out.println(name + ", you are wrong. You guessed " + maybeNumber + ". You have " + (3 - tries) + " tries left");
+            if (maybeNumber < realNumber){System.out.println(name + ", you are wrong. You guessed too low. You have " + (3 - tries) + " tries left");
                 tries++;
                 getGuess();
                 checkNumber(tries);
+            }
+            else if(maybeNumber > realNumber) {
+                System.out.println(name + ", you are wrong. You guessed too high. You have " + (3 - tries) + " tries left");
+                tries++;
+                getGuess();
+                checkNumber(tries);
+            }
         }else if(maybeNumber == realNumber) {
             if (tries == 1) {
-                System.out.println(name + ", you are right! You guessed " + maybeNumber + ", and that is the real number! It only took you " + (tries) + " try!");
-            } else {
-                System.out.println(name + ", you are right! You guessed " + maybeNumber + ", and that is the real number! It only took you " + (tries) + " tries!");
+                System.out.println(name + ", you are right! You guessed " + maybeNumber + ", and that is the real number! It only took you " + (tries) + " try! Play again? (y/n)");
+                playAgain = scan.next().equals("y");
+            } 
+            else {
+                System.out.println(name + ", you are right! You guessed " + maybeNumber + ", and that is the real number! It only took you " + (tries) + " tries! Play again? (y/n)");
+                playAgain = scan.next().equals("y");
             }
         } 
         
         else {
-            System.out.println("Sorry, but you are out of tries.");
-        }
-    }
-
-
-
-
-
-
+            System.out.println("Sorry, but you are out of tries. Play again? (y/n)");
+            playAgain = scan.next().equals("y");
+            }
+            }
+        
 }
