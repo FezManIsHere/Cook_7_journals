31917549a87e04adad4d8714032b7c0d7ca12e7c
Carter Cook <alonzi.alonzo7@gmail.com>
2015-11-20 15:49:39 -0600

    Updates

--- NetBeansProjects/Cook_7_interactivefiction/src/cook_7_interactivefiction/Cook_7_interactivefiction.java
+++ NetBeansProjects/Cook_7_interactivefiction/src/cook_7_interactivefiction/Cook_7_interactivefiction.java
@@ -12,7 +12,7 @@ public class Cook_7_interactivefiction {
     public static void intro() {
         System.out.println("This is a game not for the weak-minded. Be warned");
         System.out.println("");
-    } //Intro...simple enough
+    } //Intro...simple enough #method1()
 
     public static void rules() {
         System.out.println("Some basic rules:");
@@ -21,12 +21,12 @@ public class Cook_7_interactivefiction {
         System.out.println("Second, this game CAN be solved without knowing anything, it just takes patience, creative thinking, and a little bit of rage.");
         System.out.println("Third, mistakes will loop you back through the starting scene. Yes, you have to restart every time you answer wrong, however far you may have gotten.");
         System.out.println("");
-    } //Just some rules to help you understand how the game is played
+    } //Just some rules to help you understand how the game is played #method2()
 
     public static void main(String[] args) {
         name();
         playAgain = true;
-        while (playAgain) {
+        while (playAgain) { //while loop to repeat game #while
             playAgain = false;
             intro();
             rules();
@@ -34,9 +34,9 @@ public class Cook_7_interactivefiction {
         } //while loop to allow you to keep playing the game, assuming you win, of course
     }
 
-    public static void game() {
+    public static void game() { //actual game #method3()
 
-        System.out.println("You wake up in a strange room"); //first choice (scenario)
+        System.out.println("You wake up in a strange room"); //first choice (scenario) #hero
         answer = scan.nextLine().toLowerCase();
         if (answer.contains("smell")) {
             System.out.println("You stand up"); //second choice
@@ -45,7 +45,7 @@ public class Cook_7_interactivefiction {
                 System.out.println("You find a light switch and turn it on"); //third choice
                 answer = scan.nextLine().toLowerCase();
                 if (answer.contains("look")) {
-                    System.out.println("You see a table, your bed, and a giant monster with his hands covering his eyes"); //fourth choice
+                    System.out.println("You see a table, your bed, and a giant monster with his hands covering his eyes"); //fourth choice #enemyobject
                     answer = scan.nextLine().toLowerCase();
                     if (answer.equals("hi")) {
                         System.out.println("You say Hi to the monster, and he offers you breakfast"); //fifth choice
@@ -81,19 +81,46 @@ public class Cook_7_interactivefiction {
                                                             System.out.println("Taking the left fork, you keep going. You find two tunnels, one dark, one light"); //Choice 14a
                                                             answer = scan.nextLine().toLowerCase();
                                                             if(answer.contains("dark")) {
-                                                                System.out.println("As you go through the dark tunnel, there are bad smells. Turn back?");
-                                                                
-                                                            } else {
+                                                                System.out.println("As you go through the dark tunnel, there are bad smells. Turn back?"); //choice 15a
+                                                                answer = scan.nextLine().toLowerCase();
+                                                                if(answer.contains("dark") || answer.contains("no") || answer.equals("n")) {
+                                                                    System.out.println("You keep going, and there are creepy noises. Turn back?"); //choice 16a
+                                                                    answer = scan.nextLine().toLowerCase();
+                                                                    if (answer.contains("no") || answer.equals("n")) {
+                                                                        System.out.println("You go on. Up ahead, there is a creepy goop. Turn back?"); //choice 17a
+                                                                        answer = scan.nextLine().toLowerCase();
+                                                                        if (answer.contains("yes") || answer.equals("y")) { //#forthewin
+                                                                            System.out.println("You go back, and see a spinoff tunnel, with a light at the end. You head on in, and walk for what feels "
+                                                                                    + "like miles, until you finally get out. You turn around and see a sign that says "
+                                                                                    + "\"Vermont State Mental Hospital\" You win. The End"); //The point when you realize that you are crazy #win
+                                                                            ask();
+                                                                        } else { //bad choice (17a)
+                                                                            System.out.println("You keep going, straight into the giant slug's layer. His goop burns "
+                                                                                    + "your skin, and you slowly dissolve to death");
+                                                                            ask();
+                                                                        }
+                                                                    } else { //bad choice (16a)
+                                                                        System.out.println("You go back, to the very beginning");
+                                                                        bad();
+                                                                    }
+                                                                } else { //bad choice (15a)
+                                                                    System.out.println("You go back, to the very beginning");
+                                                                    bad();
+                                                                }
+                                                            } else { //Losing ending choice (14a)
                                                                 System.out.println("You decide just to go down the light tunnel. At the end of it, there is a monster who is eating dinner."
-                                                                        + " \"Oh,\" he said, \"Desert...gooood\"");
+                                                                        + " \"Oh,\" he said, \"Dessert...gooood\"");
+                                                                ask();
                                                             }
-                                                        } else {
+                                                        } else { //Losing ending choice (13a)
                                                             System.out.println("A snake jumps out of a bush and viciously attacks you");
+                                                            ask();
                                                         }
-                                                    } else if (answer.contains("right")) { //Losing ending choice (12)
+                                                    } else if (answer.contains("right")) { //Losing ending choice (12) #forthelose
                                                         System.out.println("As you head toward the tall fence, a man tells you to stand down. Not understanding, you keep walking"
                                                                 + " towards him. He warns you again, and you run at him to stop him before he hurts someone. He guns you down, and you lose."
-                                                                + " The End.");
+                                                                + " The End."); //#lose
+                                                        ask();
                                                     } else { //bad choice (12)
                                                         System.out.println("Being the indecisve person you are, you stand there until you melt into a liquidy puddle");
                                                         bad();
@@ -156,7 +183,7 @@ public class Cook_7_interactivefiction {
         }
     }
 
-    public static void clear() { //to save lines and give me an extra method :D
+    public static void clear() { //to save lines and give me an extra method :D #method4()
         System.out.println("");
         System.out.println("");
         System.out.println("");
@@ -164,13 +191,22 @@ public class Cook_7_interactivefiction {
         System.out.println("");
     }
 
-    public static void name() { //I call it but it is really just a physcological trick to make them think that if I care enough to get their name then I care enough to make a quality game for them and they should play it.
+    public static void name() { //I call it but it is really just a physcological trick to make them think that if I care enough to get their 
+        //name then I care enough to make a quality game for them and they should play it. #method5()
         System.out.println("What is your name?");
         name = scan.nextLine();
     }
 
-    public static void bad() { //to condense statements, requiring less room
+    public static void bad() { //to condense statements, requiring less room #method6()
         clear();
         game();
+    }
+    public static void ask() { //to ask if they want to play again #method7()
+        System.out.println("");
+        System.out.println("Play again? Y/N");
+        answer = scan.nextLine().toLowerCase();
+        if (answer.equals("y")) {
+            playAgain = true;
+        }
     }
 }
