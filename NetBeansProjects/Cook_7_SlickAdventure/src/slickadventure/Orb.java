7b585ee73ad1b8984360ed974685911a41d44395
Carter Cook <alonzi.alonzo7@gmail.com>
2016-01-28 16:12:27 -0600

    Updates

--- NetBeansProjects/Cook_7_SlickAdventure/src/slickadventure/Orb.java
+++ NetBeansProjects/Cook_7_SlickAdventure/src/slickadventure/Orb.java
@@ -5,10 +5,29 @@
  */
 package slickadventure;
 
+import org.newdawn.slick.Image;
 /**
  *
  * @author Ramborux
  */
 public class Orb {
-    
+    private int locationX;
+    private int locationY;
+    private int size;
+    private boolean isVisible;
+    Image Orb;
+    public Orb(int x, int y) {
+        this.isVisible = false;
+        this.locationX = x;
+        this.locationY = y;
+    }
+    /*
+    Getters and setters are a common concept in Java,
+    a design guideline in Java, and object oriented programming
+    in general, is to encapsulate/isolate values as much as possible.
+    Getters - are methods used to query the value of instance variables.
+    this.getLocationX();
+    Setters - are methods that set values for instance variables.
+    this.setLocationX(45);
+    */
 }
